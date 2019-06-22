ba8a7062545b199edf8c11de618d9f26a85eff75
AndreyShtyrov <andriei.shtyrov@mail.ru>
2019-06-22 15:19:47 +0300

    Setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='QMData-Tools',
-    version='0.5.0.0',
+    version='v0.5.1',
     author='Andronet',
     author_email='',
     packages=['bin', 'utils', 'gaussian09', 'reanet', 'openmolcas', 'Constants', 'orca', 'Old_scripts'],
