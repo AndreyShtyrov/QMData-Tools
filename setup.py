e0c8a4a0b12eb93a99c159d14a2922080ead3d46
AndreyShtyrov <andriei.shtyrov@mail.ru>
2019-08-31 14:16:44 +0300

    add geom convertor

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import setuptools
 
 setuptools.setup(
     name='QMData-Tools',
-    version='v0.5.4',
+    version='v0.5.5',
     author='Andronet',
     author_email='',
     packages=['bin', 'utils', 'gaussian09', 'reanet', 'openmolcas', 'Constants', 'orca', 'Old_scripts'],
     url="https://github.com/AndreyShtyrov/QMData-Tools.git",
-    scripts=['bin/check_optimization', 'bin/Convert_gauss_to_molcas', 'bin/extract_orb', 'bin/lqdel',
+    scripts=['bin/check_optimization', 'bin/generate_input', 'bin/Convert_gauss_to_molcas', 'bin/extract_orb', 'bin/lqdel',
              'bin/reshape_hessian', 'bin/show_avaliable_ants', 'bin/convert_to_molden', 'bin/get_optimized_geom',
              'Old_scripts/calculate_stat_sum.py', 'Old_scripts/Calc_freq.py', 'Old_scripts/diff_hessian.py'],
     license='',
-    install_requires=['numpy', 'termcolor', 'pathlib' ],
+    install_requires=['numpy', 'termcolor', 'pathlib', 'json' ],
     description=''
 )
