8f8950ba499a3b02b8e7d3af5a425f7b1ab9255e
AndreyShtyrov <andriei.shtyrov@mail.ru>
2019-09-02 14:26:52 +0300

    upgrate bagel input creater

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='QMData-Tools',
-    version='v0.5.12.00002',
+    version='v0.5.12.00003',
     author='Andronet',
     author_email='',
     packages=['bin', 'utils', 'gaussian09', 'reanet', 'openmolcas', 'Constants', 'orca', 'Old_scripts', 'BAGEL',
